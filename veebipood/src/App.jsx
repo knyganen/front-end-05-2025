2ce9d8d4090c6f0f4b2d7211aadb630b49cc3715
knyganen <katre_nyganen@hotmail.com>
2025-05-13 18:08:52 +0300

    Save morning class work before starting homework

--- veebipood/src/App.jsx
+++ veebipood/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Link, Route, Routes } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <img className="pilt" src="https://www.bellaandduke.com/wp-content/uploads/2024/10/Border-collie-owners-dog-breed-guide-1.png" alt="" />
-    
-    
-    <button 
-    className="nupp">Ostukorvi</button>
-    
+      <Link to="/">
+        <img className="pilt" src="https://www.bellaandduke.com/wp-content/uploads/2024/10/Border-collie-owners-dog-breed-guide-1.png" alt="" />
+      </Link>
+
+      <Link to="/ostukorv">
+        <button className="nupp">Ostukorvi</button>
+      </Link>
+
+      <Link to="/osta-kinkekaart">
+        <button className="nupp">Kinkekaardid</button>
+      </Link>
+
+      <Link to="/esindused">
+        <button className="nupp">Esindused</button>
+      </Link>
+
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        count is {count}
         </button>
+
+      <Routes>
+        <Route path= '/' element={ <div>Avalehe leht</div>} />
+        <Route path= '/ostukorv' element={ <div>Ostukorvi leht</div>} />
+        <Route path= '/osta-kinkekaart' element={ <div>Kinkekaardi leht</div>} />
+        <Route path= '/esindused' element={ <div>Esinduse leht</div>} />
+      </Routes>
         
     </>
   )
