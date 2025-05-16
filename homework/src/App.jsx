707133f4982ea84c6a60581b6ceab2a5c6dc4eeb
knyganen <katre_nyganen@hotmail.com>
2025-05-16 20:46:13 +0300

    Re-add cleaned homework folder

---
+++ homework/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button className='nupp'>nupp</button>
+
+      <img 
+        className="pilt" 
+        src="https://www.w3schools.com/css/img_5terre.jpg" 
+        alt="pilt" 
+      />
+
+      <p className="sinine">sinine</p>
+      <p className="roheline">roheline</p>
+
+
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+      
+
+      <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+      </iframe>
+
+      
+    </>
+  );
+}
+
+export default App;
