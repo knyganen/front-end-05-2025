885fc6b40da32c7c141f034faae4bdfa08e79639
knyganen <katre_nyganen@hotmail.com>
2025-05-16 19:41:58 +0300

    Add Meist and Kontakt pages

--- uudised/src/pages/Kontakt.jsx
+++ uudised/src/pages/Kontakt.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react"
 
 function Kontakt() {
+  const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false);
+  const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false);
+  const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false);
   return ( <div>
 
 <div>See on kontaktide leht, nähtav localhost: 5173/kontakt aadressil</div>
 <div>Võta meiega ühendust:</div>
 <br />
-<div>Kristiine keskus</div>
-<div>+34567769</div>
+<div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+{n2itaTelKristiine && <div>+34567769</div>}
 <div>Endla 45</div>
 <br />
-<div>Ülemiste keskus</div>
-<div>+34567888</div>
+<div onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus</div>
+{n2itaTelYlemiste && <div>+34567888</div>}
 <div>Suur-Sõjamäe 4</div>
 <br />
+<div onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku keskus</div>
+{n2itaTelTasku && <div>+34567000</div>}
+<div>Turu 2</div>
 
 
   </div>
--- uudised/src/pages/Meist.jsx
+++ uudised/src/pages/Meist.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 
 function Meist() {
+  const [kontakt, n2itaKontakt] = useState("");
   return (  <div>
 
     <div>See on meist leht, nähtav localhost: 5173/meist aadressil</div>
     <div>Meie töötajad:</div>
-    <br />
+    <br /> 
     <div>John Smith</div>
-    <div>+3456788</div>
     <div>Büroojuht</div>
-    <br />
+    <button onClick={() => n2itaKontakt('+3456788')}>Võta ühendust</button>
+    <br /> <br />
     <div>Kalle Blomkvist</div>
-    <div>+3567788</div>
     <div>Tõlk</div>
+    <button onClick={() => n2itaKontakt('+3456789')}>Võta ühendust</button>
+    <br /> <br />
+    <div>Maali Maailm</div>
+    <div>Sekretär</div>
+    <button onClick={() => n2itaKontakt('+3456790')}>Võta ühendust</button>
+    <br /> <br />
+    { kontakt  !== "" && <div>Tema kontakt: {kontakt}</div>}
+    
     </div>
-  ) 
-}
 
+  ) 
+
+}
 
 
 export default Meist
